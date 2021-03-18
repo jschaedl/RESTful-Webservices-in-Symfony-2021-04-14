89da55f21a7701314921f2ad44c0d7bbbac475a2
Jan Schädlich <schaedlich.jan@gmail.com>
2021-03-18 17:44:47 +0000

    coding challenge 2 solution

---
+++ src/Serializer/WorkshopNormalizer.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Serializer;
+
+use App\Entity\Attendee;
+use App\Entity\Workshop;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+final class WorkshopNormalizer implements ContextAwareNormalizerInterface
+{
+    public function __construct(
+        private ObjectNormalizer $normalizer
+    ) {
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = [])
+    {
+        return $data instanceof Workshop;
+    }
+
+    /**
+     * @param Workshop $object
+     */
+    public function normalize($object, string $format = null, array $context = [])
+    {
+        $customContext = [
+            AbstractNormalizer::IGNORED_ATTRIBUTES => ['id'],
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                if ($object instanceof Attendee) {
+                    return $object->getFirstname().' '.$object->getLastname();
+                }
+
+                if ($object instanceof Workshop) {
+                    return $object->getTitle();
+                }
+
+                return $object;
+            },
+        ];
+
+        $context = array_merge($context, $customContext);
+
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        if (\is_array($data)) {
+            $data['workshop_date'] = $object->getWorkshopDate()->format('Y-m-d');
+        }
+
+        return $data;
+    }
+}
